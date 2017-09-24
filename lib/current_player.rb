44e73f566fa158f86b9701d7c665e13b14e6e993
David Kuruvilla <dapawn@gmail.com>
2017-09-24 00:30:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   cnt = 0
-  board.each{ |position|  position != " " ? cnt += 1 : }
+  board.each{ |position|  position != " " ? cnt += 1 : "" }
   end
   return cnt
 end
 
 def current_player(board)
-  cnt.even? ? return "X" : return "O"
+  return cnt.even? ? "X" : "O"
 end
